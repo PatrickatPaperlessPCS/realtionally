b16e6ff52c2dc3a4bb18c2fbf215652588593c02
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2017-01-09 15:51:33 -0600

    removed references to honeypot, causing errors in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     before_filter :configure_permitted_parameters, if: :devise_controller?
- invisible_captcha only: [:create, :update], honeypot: :honeypot
+
 def after_sign_in_path_for(user)
   pages_dashboard_path
 end
